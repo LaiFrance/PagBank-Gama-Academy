604f6587be9eaaccf108c14f649b03174f8f51f1
Laise France <laise94@hotmail.com>
2022-10-17 06:18:22 -0300

    aula2-scanner-java

---
+++ Aula2-ScannerJava/Leitura.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String args[]) {
+        Scanner teclado = new Scanner(System.in);
+        int valorInteiro;
+        float valorFloat;
+
+        System.out.println("Por favor, digite um valor inteiro");
+        valorInteiro = teclado.nextInt();
+
+        System.out.println("Por favor, digite um valor real");
+        valorFloat = teclado.nextFloat();
+
+        System.out.println("Voce digitou os valores " + valorInteiro + " e " + valorFloat);
+        teclado.close();
+    }
+}
+
+// Path: Aula2-ScannerJava\Leitura.java
