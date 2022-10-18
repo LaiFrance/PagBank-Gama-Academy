c5bbd4b97487874b45a238def4fcc34209f2beb7
Laise France <laise94@hotmail.com>
2022-10-18 20:47:07 -0300

    exercicios feitos em aula dia 18-10

---
+++ aula-dia18-10-exercicios/Curso.java
@@ -0,0 +1,19 @@
+public class Curso {
+
+    public void escreveNome(){
+    int x =0;
+
+    while (x <= 4) {
+        System.out.println("ElasTech");
+        x = x + 1;
+    }
+}
+
+ 
+
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.escreveNome();
+} 
+    
+    }
